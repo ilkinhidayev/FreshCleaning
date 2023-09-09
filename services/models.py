734fb83081889737df1342b7972edee8807c401a
Ilkin Hidayev <ilkin.hidayev@gmail.com>
2023-09-10 01:47:23 +0400

    Added "Add service" page

--- services/models.py
+++ services/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class Service(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    date_added = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.name
--- services/views.py
+++ services/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ServiceForm
+from .models import Service
 
-# Create your views here.
+def add_service(request):
+    if request.method == 'POST':
+        form = ServiceForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')  # Ana sayfaya yönlendirme
+    else:
+        form = ServiceForm()
+
+    context = {'form': form}
+    return render(request, 'services/add_service.html', context)
