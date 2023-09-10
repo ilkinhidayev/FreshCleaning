40dbfbaf67b330ea1090a1b76ef435c650104150
Ilkin Hidayev <ilkin.hidayev@gmail.com>
2023-09-10 04:37:55 +0400

    Added profile information

--- user_panel/urls.py
+++ user_panel/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='user_dashboard'),
+    path('profile/', views.user_profile, name='user_profile'),
 ]
--- user_panel/views.py
+++ user_panel/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render, redirect
 from .models import Order
+from .forms import UserProfileForm
+from django.contrib import messages
+
 
 def dashboard(request):
     if not request.user.is_authenticated:
         return redirect('login_url')  # Burada 'login_url' kısmını kendi giriş URL'nizle değiştirin.
     user_orders = Order.objects.filter(user=request.user)
     return render(request, 'user_panel/dashboard.html', {'orders': user_orders})
+
+def user_profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profiliniz güncellendi.')
+            return redirect('user_dashboard')
+    else:
+        form = UserProfileForm(instance=request.user)
+
+    context = {'form': form}
+    return render(request, 'user_panel/profile.html', context)
